f86b7397f41e0a6c9836ceb0f7b713f55375876a
yuriy05 <dilayyura@gmail.com>
2023-11-27 22:44:45 +0200

    practice useState for form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
+import "./App.css";
+
+const FIELD_NAME = {
+  NAME: "name",
+  EMAIL: "email",
+  PASSWORD: "password",
+};
+
+const initialValues = (values) => ({
+  [FIELD_NAME.NAME]: "",
+  [FIELD_NAME.EMAIL]: "",
+  [FIELD_NAME.PASSWORD]: "",
+  ...values,
+});
 
 function App() {
+
+  const [value, setValue] = useState(() => initialValues({color: "red"}))
+
+  const [userList, setUserList] = useState([]);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUserList([value, ...userList]);
+
+    setValue(initialValues());
+  };
+
+  const handleChange = (e) => {
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(value);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              placeholder="Ім'я"
+              name={FIELD_NAME.NAME}
+              value={value[FIELD_NAME.NAME]}
+              onChange={handleChange}
+            />
+          </div>
+  
+          <div>
+          <input
+              type="email"
+              placeholder="Email"
+              name={FIELD_NAME.EMAIL}
+              value={value[FIELD_NAME.EMAIL]}
+              onChange={handleChange}
+            />
+          </div>
+  
+          <div>
+          <input
+              type="password"
+              placeholder="Пароль"
+              name={FIELD_NAME.PASSWORD}
+              value={value[FIELD_NAME.PASSWORD]}
+              onChange={handleChange}
+            />
+          </div>
+  
+          <button type="submit">Відправити</button>
+        </form>
+  
+        {userList.length > 0 && (
+          <ul>
+            {userList.map((user, index) => (
+              <React.Fragment key={user[FIELD_NAME.NAME]}>
+                <li><User {...user} show={index === 0}/></li>
+              </React.Fragment>
+            ))}
+          </ul>
+        )}  
       </header>
     </div>
   );
 }
 
-export default App;
+function User({name, email, password, show: initialShow}) {
+  const [show, setShow] = useState(initialShow);
+
+  const toggleDetails = () => {
+    setShow(!initialShow);
+  };
+
+  const isShow = initialShow && show
+
+  return (
+    <React.Fragment>
+      <div onClick={toggleDetails}>
+        {name}
+      </div>
+
+      {isShow && (
+        <ul>
+          <li>Email: {email}</li>
+          <li>Password: {password}</li>
+        </ul>
+      )}
+    </React.Fragment>
+  )
+}
+
+export default App
